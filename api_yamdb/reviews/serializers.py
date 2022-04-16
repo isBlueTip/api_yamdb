da920bfcba2dd5c874ff168b02bc1efb8499ba5e
Юлия Васильева <julka3561@gmail.com>
2022-04-16 11:04:12 +0300

    Base viewsets Review and Comment, Pagination settings.

--- api_yamdb/reviews/serializers.py
+++ api_yamdb/reviews/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
-from reviews.models import Review
+from reviews.models import Comment, Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -10,3 +10,11 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ['id', 'text', 'author', 'score', 'pub_date']
         model = Review
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = ['id', 'text', 'author', 'pub_date']
+        model = Comment
--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
-from reviews.serializers import ReviewSerializer
+from reviews.serializers import CommentSerializer, ReviewSerializer
 from titles.models import Title
 
 
@@ -9,9 +9,23 @@ class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
 
     def get_queryset(self):
-        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
-        return title.reviews.all()
+        self.title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        return self.title.reviews.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+        serializer.save(title=self.title)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    ...
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        self.review = get_object_or_404(title.reviews.all(),
+                                        pk=self.kwargs.get('review_id'))
+        return self.review.comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+        serializer.save(review=self.review)
