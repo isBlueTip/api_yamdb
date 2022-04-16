20ce883d6b91e83e12bea324a554ca3a3c9ac7d5
Юлия Васильева <julka3561@gmail.com>
2022-04-16 22:58:17 +0300

    Added POST functionality to viewsets

--- api_yamdb/api/reviews_views.py
+++ api_yamdb/api/reviews_views.py
@@ -3,29 +3,28 @@ from rest_framework import viewsets
 
 from api.reviews_serializers import CommentSerializer, ReviewSerializer
 from titles.models import Title
+from reviews.models import Review
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
 
     def get_queryset(self):
-        self.title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
-        return self.title.reviews.all()
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        return title.reviews.all()
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-        serializer.save(title=self.title)
+        serializer.save(author=self.request.user,
+                        title_id=self.kwargs.get('title_id'))
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
 
     def get_queryset(self):
-        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
-        self.review = get_object_or_404(title.reviews.all(),
-                                        pk=self.kwargs.get('review_id'))
-        return self.review.comments.all()
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        return review.comments.all()
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-        serializer.save(review=self.review)
+        serializer.save(author=self.request.user,
+                        review_id=self.kwargs.get('review_id'))
