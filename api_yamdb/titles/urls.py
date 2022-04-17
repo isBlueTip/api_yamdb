c4ba3a89049f5b879646285b4388903b43708548
Simon Egorov <rhinoRofl@gmail.com>
2022-04-17 19:21:57 +0700

    First commit: new files added

---
+++ api_yamdb/titles/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from titles.views import CategoryViewSet, GenreViewSet, TitleViewSet
+
+router = routers.DefaultRouter()
+
+router.register("categories", CategoryViewSet, basename="categories")
+router.register("genres", GenreViewSet, basename="genres")
+router.register("titles", TitleViewSet, basename="titles")
+
+urlpatterns = [
+    path("/", include(router.urls)),
