abd45ddd8ac53f77ecf9de859366264652478c7b
Simon Egorov <rhinoRofl@gmail.com>
2022-04-17 18:03:16 +0700

    Reorder paths

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -8,7 +8,7 @@ router = SimpleRouter()
 router.register('', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('signup/', SignupView.as_view(), name='request_confirmation'),
     path('token/', TokenView.as_view(), name='obtain_token'),
+    path('', include(router.urls)),
 ]
