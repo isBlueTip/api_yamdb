4660be107e6ebfd982ee5825965add5ed0544919
Simon Egorov <rhinoRofl@gmail.com>
2022-04-21 21:36:03 +0700

    Replace if-clause in users_views

--- api_yamdb/api/users_views.py
+++ api_yamdb/api/users_views.py
@@ -27,27 +27,6 @@ file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
 
-# def send_otp(email):
-#     key = randint(99999, 999999)
-#     send_mail(
-#         'Регистрация нового пользователя',
-#         f'Ваш код подтверждения: {key}.'
-#         'Используйте его для авторизации.',
-#         'yamdb@example.com',  # 'from' field
-#         [f'{email}'],  # 'to' field
-#         fail_silently=False,
-#     )
-#     return key
-#
-#
-# def get_tokens_for_user(user):
-#     refresh = RefreshToken.for_user(user)
-#
-#     return {
-#         'access': str(refresh.access_token),
-#     }
-
-
 class SignupView(APIView):
     """View to register a new user and verify email."""
 
@@ -55,12 +34,11 @@ class SignupView(APIView):
 
     def post(self, request):
         serializer = SignupSerializer(data=request.data)
-        if serializer.is_valid():
-            email = request.data.get('email')
-            otp = send_otp(email)
-            serializer.save(confirmation_code=otp)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        email = request.data.get('email')
+        otp = send_otp(email)
+        serializer.save(confirmation_code=otp)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class TokenView(APIView):
@@ -70,12 +48,11 @@ class TokenView(APIView):
 
     def post(self, request):
         serializer = TokenSerializer(data=request.data)
-        if serializer.is_valid():
-            user = get_object_or_404(User,
-                                     username=request.data.get('username'))
-            token = get_tokens_for_user(user)
-            return Response(token, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        user = get_object_or_404(User,
+                                 username=request.data.get('username'))
+        token = get_tokens_for_user(user)
+        return Response(token, status=status.HTTP_201_CREATED)
 
 
 class UserViewSet(viewsets.ModelViewSet):
